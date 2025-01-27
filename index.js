945ef7f79ae18703f92139580b1137bb8b9e2e54
vandit <jaiswalvandit25@gmail.com>
2025-01-27 21:31:11 +0530

    NewfeatureBadded

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function newName(){
     console.log("new name added")
 }
+
+function newFeature(){
+    console.log("New Feature")
+}
+
+function newFeatureA(){
+    console.log("New Feature A")
+}
+
+function newFeatureB(){
+    console.log("New featuer B")
+}
